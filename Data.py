c087ca1bc213cf432a11a33ee4cbda49fb4cf4d9
Ferran Álvarez <87202859+ferranad05@users.noreply.github.com>
2021-07-09 20:13:53 +0200

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,14 @@
+name = "John"
+age = "8"
+print("There once was a man named "+name+",")
+print("he was "+age+" years old.")
+
+name = "Pepe"
+print("He really liked the name "+name+",")
+print("but didn't like being "+age+".")
+
+#Notes
+
+name = "Paul"  #Strings
+number = 60    #Numbers
+is_male = True #True or False values
